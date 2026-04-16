62de4b45515ae32b582e958b351a58afb164489e
henry232323 <henry232323@users.noreply.github.com>
2026-04-15 18:39:25 -0700

    revert: revert to last known good state, remove subscribe cog

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ from cogs.lookup import Lookup
 from cogs.order import order
 from cogs.registration import Registration
 from cogs.stock import stock
-from cogs.subscribe import AlertSubscriptions
 
 from util.config import Config
 from util.result import Result
@@ -42,7 +41,6 @@ class SCMarket(Bot):
         await self.add_cog(Lookup(self))
         await self.add_cog(order(self))
         await self.add_cog(stock(self))
-        await self.add_cog(AlertSubscriptions(self))
 
         await self.tree.sync()
 
